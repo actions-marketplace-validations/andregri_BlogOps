298bd42854b3b5e851929f406d158d43a9dcf7ff
andregri <andrea.grillo96@live.com>
2022-03-15 23:20:25 +0100

    Get repo from PyGithub

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
 import glob
+from dotenv import load_dotenv
+from github import Github
 
 
 def local_posts_state(posts_path='./_posts/'):
     posts = glob.glob(posts_path + "/*.md")
     return posts
 
+def get_last_commit(repo_name):
+    #TODO
+    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+    gh = Github(access_token)
+    
+    repo = gh.get_user().get_repo(repo_name)
+    print(repo)
+
 
 def main():
-    posts_path = os.environ["INPUT_POSTSPATH"]
+    load_dotenv()
+
+    get_last_commit("BlogOps")
+    
+    #posts_path = os.environ["INPUT_POSTSPATH"]
+    posts_path = "tests/posts"
 
     posts = local_posts_state(posts_path)
     my_output = f"Posts path: {posts_path}"
