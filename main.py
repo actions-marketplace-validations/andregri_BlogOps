e915e0d2a97a0c56fae96eec826f208045a5f439
andregri <andrea.grillo96@live.com>
2022-03-19 17:27:06 +0100

    Get new files from commit

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import os
-import glob
 from dotenv import load_dotenv
 from github import Github
-
-
-def local_posts_state(posts_path='./_posts/'):
-    posts = glob.glob(posts_path + "/*.md")
-    return posts
+from pathlib import Path
 
 def get_last_commit(repo_name):
-    #TODO
+    """Return last commit from repository `repo_name`"""
     access_token = os.getenv("GITHUB_ACCESS_TOKEN")
     gh = Github(access_token)
     
     repo = gh.get_user().get_repo(repo_name)
-    print(repo)
+    commit = repo.get_commits()[0] # latest commit
+    return commit
 
+def get_files_in(files, dir):
+    """Return a subset of `files` in `dir` path"""
+    new_files = []
+    for file in files:
+        if Path(dir) in Path(file.filename).parents:
+            new_files.append(file)
+    return new_files
+
+def get_new_files(files, status):
+    """Return a subset of `files` with this `status`"""
+    new_files = []
+    for file in files:
+        if file.status == "added":
+            new_files.append(file)
+    return new_files
 
 def main():
     load_dotenv()
 
-    get_last_commit("BlogOps")
-    
-    #posts_path = os.environ["INPUT_POSTSPATH"]
-    posts_path = "tests/posts"
+    REPO_NAME = os.getenv("REPO_NAME")
+    POSTS_DIR = os.getenv("INPUT_POSTS_DIR")
 
-    posts = local_posts_state(posts_path)
-    my_output = f"Posts path: {posts_path}"
-    for post in posts:
-        my_output += f'\n{post}'
+    last_commit = get_last_commit(REPO_NAME)
+    new_files = get_files_in(last_commit.files, POSTS_DIR)
+    added_files = get_new_files(new_files, status="added")
+
+    my_output = f"{len(added_files)} new files in {POSTS_DIR}:"
+    for file in added_files:
+        my_output += f'\n{file}'
 
     print(f"::set-output name=myOutput::{my_output}")
 
