fadf66d7f297f8ad464a70b4d77d8d6f1aa52180
andregri <andrea.grillo96@live.com>
2022-05-08 18:16:07 +0200

    Publish new files to medium

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from cmath import exp
 import os
 from dotenv import load_dotenv
 from github import Github
 from pathlib import Path
+import pymediumapi
+import requests
+import base64
 
 
 def get_last_commit(repo_name):
@@ -14,18 +18,57 @@ def get_last_commit(repo_name):
     return commit
 
 
+def get_new_files(commit, dir):
+    """Return new files in commit that are in dir directory"""
+    files = []
+    for file in commit.files:
+        if Path(dir) in Path(file.filename).parents and file.status == "added":
+            files.append(file)
+    return files
+
+
 def main():
     load_dotenv()
 
     REPO_NAME = os.getenv("REPO_NAME")
     POSTS_DIR = os.getenv("INPUT_POSTS_DIR")
 
+    # Get modified file in the last commit
     last_commit = get_last_commit(REPO_NAME)
+    files = get_new_files(last_commit, POSTS_DIR)
+
+    # Create mediumAPI client
+    token = os.getenv("MEDIUM_INTEGRATION_TOKEN")
+    client = pymediumapi.Client(token)
+    client.authenticate()
 
     my_output = ""
-    for file in last_commit.files:
-        if Path(POSTS_DIR) in Path(file.filename).parents and file.status == "modified":
-            my_output += f" {file.filename}"
+
+    for file in files:
+        # Download file content
+        response = requests.get(file.contents_url)
+        response_json = response.json()
+        content_base64 = response_json["content"]
+        content = base64.b64decode(content_base64).decode('utf-8')
+
+        # Find title from parameters at the beginning of the file
+        title_start_idx = content.find("title:") + len("title:")
+        title_end_idx = content.find('\n', title_start_idx)
+        title = content[title_start_idx:title_end_idx].lstrip()
+
+        # Prepare the text body of the post
+        text_start_idx = content.find("---\n", title_end_idx) + len("---\n")
+        body = content[text_start_idx:]
+        
+        try:
+            client.create_post(
+                title=title,
+                content_format='markdown',
+                content=body
+            )
+            my_output += f"Published {file.filename}\n"
+        except RuntimeError as e:
+            my_output.append(f"Could not publish {file.filename}")
 
     print(f"::set-output name=myOutput::{my_output}")
 
